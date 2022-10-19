75ab42c519201d5356d5a9b302aa8c4fb55dc55b
winnie-woo <hanxuwu0428@gmail.com>
2022-10-18 22:26:06 -0700

    upddate

--- sketch.js
+++ sketch.js
@@ -21,26 +21,27 @@ let Bunnies = [{
 },];
 
 let randomIndex;
-let animating = false;
+let animating = true;
 
 function setup() {
     createCanvas(400, 400);
     background(255, 255, 183);
     textSize(32);
-    text("click to randomize",50,50);
+    text("cheese page",50,50);
   }
   
 
   function draw() {
 
-    if(animating == ture){
-      ellipse(random(width),random(height),random(50,200));
+    if(animating == true){
+      ellipse(random(width),random(height),random(50,50));
+  
     }
   }
 
   
 function changeBackground(){
-  if (counter<= 5){
+  if (counter<= 6){
     counter++;
     console.log(counter)
   background(random(255),random(255),random(255));
@@ -51,22 +52,24 @@ function changeBackground(){
 
 function randomizer(){
   animating = false;
+
  if (Bunnies [0]){
  background(random(200,255));
  randomIndex= int(random(Bunnies.Length));
           //console.log(Bunnies.Length);
           //console.log(Bunnies[int(random(Bunnies.length))].name);
           //console.log(Bunnies[randomIndex].name);
- text(Bunnies[randomIndex].name,50,50);
+
+ text(Bunnies[randomIndex].name + "'s favorite color is" + Bunnies[randomIndex].color,50,50);
  Bunnies.splice(randomIndex,1);
  } else {
  background(random(200,255));
  text("nothing left!",50,50);
  }
 }
-
+ 
 function mousePressed(){
- animating = ture;
+ animating = true;
  setTimeout(randomizdr,2000);
-
+console.log("beep")
 }
